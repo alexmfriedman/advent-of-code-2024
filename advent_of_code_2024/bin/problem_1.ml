6366c24b55f5fc7de82ac781ea40fe43cae48780
Alex Friedman <amfriedman27@gmail.com>
2024-12-21 11:38:00 -0400

    delete initial problem 1 comment

--- advent_of_code_2024/bin/problem_1.ml
+++ advent_of_code_2024/bin/problem_1.ml
@@ -19,13 +19,6 @@ let run ~filename =
                | _ -> failwith "bad");
                Deferred.unit))
   in
-  (* let l1 = Queue.to_array l1 in
-  let l2 = Queue.to_array l2 in
-  Array.sort l1 ~compare:Int.compare;
-  Array.sort l2 ~compare:Int.compare;
-  let result =
-    Array.fold2_exn l1 l2 ~init:0 ~f:(fun acc e1 e2 -> acc + Int.abs (e2 - e1))
-  in  *)
   let right_list_counts =
     Queue.to_list l2 |> Sequence.of_list
     |> Sequence.map ~f:(fun elem -> (elem, 1))
