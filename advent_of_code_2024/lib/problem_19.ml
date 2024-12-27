ffdc922a1b06e01bda8c324de47ca8b78f78bea9
Alex Friedman <amfriedman27@gmail.com>
2024-12-26 19:15:30 -0500

    problem 19 pt 2

--- advent_of_code_2024/lib/problem_19.ml
+++ advent_of_code_2024/lib/problem_19.ml
@@ -1,9 +1,27 @@
 open! Core
 open! Async
 
-let construct_re ~towels =
-  let towel_options = String.concat ~sep:"|" towels in
-  sprintf "^(%s)*$" towel_options
+let towel_matches_pattern_substring ~towel ~target ~start_idx =
+  start_idx + String.length towel <= String.length target
+  && String.for_alli towel ~f:(fun i c ->
+    Char.equal c (String.get target (start_idx + i)))
+;;
+
+let num_possible_designs ~towels ~target =
+  let dp =
+    Memo.recursive ~hashable:Int.hashable (fun dp idx ->
+      if idx = String.length target
+      then 1
+      else
+        List.sum
+          (module Int)
+          towels
+          ~f:(fun towel ->
+            if towel_matches_pattern_substring ~towel ~target ~start_idx:idx
+            then dp (idx + String.length towel)
+            else 0))
+  in
+  dp 0
 ;;
 
 let run ~filename =
@@ -11,16 +29,9 @@ let run ~filename =
   let towels =
     List.hd_exn file_lines |> String.split ~on:',' |> List.map ~f:String.strip
   in
-  let re_string = construct_re ~towels in
-  let re = Re.Pcre.re re_string |> Re.compile in
-  let patterns = List.drop file_lines 2 in
+  let targets = List.drop file_lines 2 in
   let num_possible_designs =
-    List.sum
-      (module Int)
-      patterns
-      ~f:(fun pattern ->
-        print_s ([%sexp_of: string * string] (re_string, pattern));
-        Re.all re pattern |> List.length)
+    List.sum (module Int) targets ~f:(fun target -> num_possible_designs ~towels ~target)
   in
   print_s ([%sexp_of: int] num_possible_designs)
 ;;
